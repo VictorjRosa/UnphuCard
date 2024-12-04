d15e1444b908480430a13884dd23738fdca77248
rosmarystosj <rosmarystosj@gmail.com>
2024-12-03 21:20:54 -0400

    jj

---
+++ UnphuCard_RecargaFront/Data/UsuarioService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Json;
+using UnphuCard_Api.DTOS;
+using UnphuCard_Api.Models;
+
+namespace UnphuCard_RecargaFront.Data
+{
+  
+
+    public class UsuarioService
+    {
+        private readonly HttpClient _httpClient;
+
+        public UsuarioService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Usuario> GetStudentInfoAsync(int id)
+        {
+            var response = await _httpClient.GetAsync($"api/MostrarUsuario/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<Usuario>();
+            }
+            else
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                throw new ApplicationException($"Error al obtener información del estudiante: {errorMessage}");
+            }
+        }
+    }
+
+}
---
+++ UnphuCard_RecargaFront/Services/AuthMessageHandler.cs
@@ -0,0 +1,31 @@
+﻿using Blazored.LocalStorage;
+using System.Net.Http.Headers;
+
+namespace UnphuCard_RecargaFront.Services
+{
+    public class AuthMessageHandler : DelegatingHandler
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public AuthMessageHandler(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // Obtener el token de local storage
+            var token = await _localStorage.GetItemAsync<string>("authToken");
+
+            // Si el token existe, lo agregamos a la cabecera Authorization
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            // Continuar con la solicitud
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+
+}
