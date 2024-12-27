5412c35070af32b064af5ada4437ddd1bdc87228
rosmarystosj <rosmarystosj@gmail.com>
2024-12-27 15:31:33 -0400

    ros

--- UnphuCard_PagosFront/Data/InventarioService.cs
+++ UnphuCard_PagosFront/Data/InventarioService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using UnphuCard_Api.DTOS;
 using UnphuCard_Api.Models;
 
 public class InventarioService
@@ -12,17 +13,28 @@ public class InventarioService
         _httpClient = httpClient;
     }
 
-    public async Task<VwInventarioEstablecimiento?> GetInventarioEstablecimientoAsync(int id)
+  
+    public async Task<List<VwInventarioEstablecimiento>> GetInventarioEstablecimientoAsync(int id)
     {
-        try
-        {
-            var url = $"api/ObtenerInvEstablecimiento/{id}";
-            return await _httpClient.GetFromJsonAsync<VwInventarioEstablecimiento>(url);
-        }
-        catch (HttpRequestException ex)
-        {
-            throw new ApplicationException($"Error al obtener el inventario: {ex.Message}", ex);
-        }
+        var response = await _httpClient.GetFromJsonAsync<List<VwInventarioEstablecimiento>>($"api/ObtenerInvEstablecimiento/{id}");
+        return response ?? new List<VwInventarioEstablecimiento>();
+    }
+
+    public async Task<bool?> RegistrarInventario(InsertInventario insertInventario)
+    {
+        
+            var response = await _httpClient.PostAsJsonAsync("api/Registrarinventario", insertInventario);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                throw new ApplicationException(errorMessage);
+            }
+   
     }
 }
 
